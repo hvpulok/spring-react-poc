cb0a255318d26df0d5d1d25d5ccfcabb95b709b3
hvpulok <hvpulok@gmail.com>
2019-08-21 22:51:25 -0400

    initial commit

---
+++ src/main/java/com/muminlife/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,17 @@
+package com.muminlife.cardatabase;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+		logger.info("Hello Spring Boot");
+	}
+
+}
