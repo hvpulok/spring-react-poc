1d95a6059ca8a090b4d6f9b9b08af0c316a2baf3
hvpulok <hvpulok@gmail.com>
2019-08-22 22:27:29 -0400

    Add car owner repository

--- src/main/java/com/muminlife/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/muminlife/cardatabase/CardatabaseApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 
 import com.muminlife.cardatabase.domain.Car;
 import com.muminlife.cardatabase.domain.CarRepository;
+import com.muminlife.cardatabase.domain.Owner;
+import com.muminlife.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -17,18 +19,30 @@ public class CardatabaseApplication {
 	@Autowired
 	private CarRepository repository;
 
+	@Autowired
+	private OwnerRepository orepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
-		logger.info("Hello Spring Boot");
+		logger.info("Spring Boot started successfully");
 	}
 
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			logger.info("Saving demo data to database");
-			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000));
-			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000));
-			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+			logger.info("Adding mock owners to database");
+			Owner owner1 = new Owner("John", "Johnson");
+			Owner owner2 = new Owner("Mary", "Robinson");
+			orepository.save(owner1);
+			orepository.save(owner2);
+
+			logger.info("Adding mock cars to database");
+			Car car = new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1);
+			repository.save(car);
+			car = new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2);
+			repository.save(car);
+			car = new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2);
+			repository.save(car);
 		};
 	}
 
--- src/main/java/com/muminlife/cardatabase/domain/Car.java
+++ src/main/java/com/muminlife/cardatabase/domain/Car.java
@@ -1,30 +1,37 @@
 package com.muminlife.cardatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
-	  @Id
-	  @GeneratedValue(strategy=GenerationType.AUTO)
-	  private long id;
-	  private String brand, model, color, registerNumber;
-	  private int year, price;
-	  
-	  public Car() {}
-	  
-	  public Car(String brand, String model, String color, 
-	    String registerNumber, int year, int price) {
-	    super();
-	    this.brand = brand;
-	    this.model = model;
-	    this.color = color;
-	    this.registerNumber = registerNumber;
-	    this.year = year;
-	    this.price = price;
-	  }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String brand, model, color, registerNumber;
+	private int year, price;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
+
+	public Car() {
+	}
+
+	public Car(String brand, String model, String color, String registerNumber, int year, int price, Owner owner) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+		this.registerNumber = registerNumber;
+		this.year = year;
+		this.price = price;
+		this.owner = owner;
+	}
 
 	public String getBrand() {
 		return brand;
@@ -73,5 +80,13 @@ public class Car {
 	public void setPrice(int price) {
 		this.price = price;
 	}
-	  
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
+	}
+
 }
