0bc7606951bbf9a6bc2c1a96e04fbdd6c3b841bd
hvpulok <hvpulok@gmail.com>
2019-08-25 19:40:39 -0400

    client: Update app component

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Car database</h1>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <div className="App">
+          <h1>Car database</h1>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+        </div>
+      </React.Fragment>
     )
   }
 }
