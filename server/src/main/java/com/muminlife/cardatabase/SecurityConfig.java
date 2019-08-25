45263558ea0ce6496e16d9c70f62f928c7014c7f
hvpulok <hvpulok@gmail.com>
2019-08-25 19:06:32 -0400

    server: temporarily disable auth filtering

--- server/src/main/java/com/muminlife/cardatabase/SecurityConfig.java
+++ server/src/main/java/com/muminlife/cardatabase/SecurityConfig.java
@@ -31,13 +31,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll()
-				.anyRequest().authenticated().and()
-				// Filter for the api/login requests
-				.addFilterBefore(new LoginFilter("/login", authenticationManager()),
-						UsernamePasswordAuthenticationFilter.class)
-				// Filter for other requests to check JWT in header
-				.addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+//		TODO: Uncomment in prod
+//		http.csrf().disable().cors().and().authorizeRequests().antMatchers(HttpMethod.POST, "/login").permitAll()
+//				.anyRequest().authenticated().and()
+//				// Filter for the api/login requests
+//				.addFilterBefore(new LoginFilter("/login", authenticationManager()),
+//						UsernamePasswordAuthenticationFilter.class)
+//				// Filter for other requests to check JWT in header
+//				.addFilterBefore(new AuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Bean
