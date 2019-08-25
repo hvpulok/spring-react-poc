18d3431e5c420bc666f1f5af2a523745a8069afc
hvpulok <hvpulok@gmail.com>
2019-08-24 23:17:41 -0400

    Add user authentication from DB

--- src/main/java/com/muminlife/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/muminlife/cardatabase/CardatabaseApplication.java
@@ -12,6 +12,8 @@ import com.muminlife.cardatabase.domain.Car;
 import com.muminlife.cardatabase.domain.CarRepository;
 import com.muminlife.cardatabase.domain.Owner;
 import com.muminlife.cardatabase.domain.OwnerRepository;
+import com.muminlife.cardatabase.domain.User;
+import com.muminlife.cardatabase.domain.UserRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
@@ -21,6 +23,9 @@ public class CardatabaseApplication {
 
 	@Autowired
 	private OwnerRepository orepository;
+
+	@Autowired
+	private UserRepository urepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -40,6 +45,12 @@ public class CardatabaseApplication {
 			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1));
 			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2));
 			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
+
+			logger.info("Adding mock username: user password: user");
+			urepository.save(new User("user", "$2a$04$1.YhMIgNX/8TkCKGFUONWO1waedKhQ5KrnB30fl0Q01QKqmzLf.Zi", "USER"));
+			logger.info("Adding mock username: username: admin password: admin");
+			urepository
+					.save(new User("admin", "$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG", "ADMIN"));
 		};
 	}
 
