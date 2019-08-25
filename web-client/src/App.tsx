072f509dba6a85fe7cd4102087f0ddf18ed9ff9a
hvpulok <hvpulok@gmail.com>
2019-08-25 19:48:04 -0400

    client: Add nav bar component

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
+import NavBar from './components/nav-bar/NavBar';
 import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <React.Fragment>
         <CssBaseline />
         <div className="App">
+          <NavBar />
           <h1>Car database</h1>
           <Button variant="contained" color="primary">
             Hello World
