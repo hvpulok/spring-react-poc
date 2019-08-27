61e165f58a704c7e144188beed1ad71794a6ddd4
hvpulok <hvpulok@gmail.com>
2019-08-27 07:47:40 -0400

    client: Add cars by calling api

---
+++ web-client/src/car/car-list.model.ts
@@ -0,0 +1,8 @@
+export default interface Car{
+    brand: string;
+    model: string;
+    color: string;
+    registerNumber: string;
+    year: number;
+    price: number;
+}
--- web-client/src/car/car-list.tsx
+++ web-client/src/car/car-list.tsx
@@ -1,14 +1,66 @@
 import * as React from 'react';
+import ReactTable from 'react-table';
+import axios from 'axios';
+import 'react-table/react-table.css';
+import Car from './car-list.model';
+import { SERVER_URL } from '../configs/app.const';
 
 export interface IAppProps {
 }
 
-export default class CarList extends React.Component<IAppProps> {
-    public render() {
-        return (
-            <div>
-                <h1>car list</h1>
-            </div>
-        );
+export interface IAppStates {
+    carList: Car[]
+}
+
+export default class CarList extends React.Component<IAppProps, IAppStates> {
+    constructor(props: IAppProps) {
+        super(props);
+        this.state = { carList: [] }
+    }
+
+    updateCarList() {
+        axios.get(`${SERVER_URL}/api/cars`)
+            .then(({ data }) => {
+                console.log(data._embedded);
+                this.setState({ carList: data._embedded.cars })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    componentDidMount() {
+        this.updateCarList();
+    }
+
+    render() {
+        const columns = [
+            {
+                Header: 'Brand',
+                accessor: 'brand' // String-based value accessors!
+            },
+            {
+                Header: 'Model',
+                accessor: 'model'
+            },
+            {
+                Header: 'Color',
+                accessor: 'color'
+            },
+            {
+                Header: 'RegisterNumber',
+                accessor: 'registerNumber'
+            },
+            {
+                Header: 'Year',
+                accessor: 'year'
+            },
+            {
+                Header: 'Price',
+                accessor: 'price'
+            },
+        ]
+
+        return <ReactTable data={this.state.carList} columns={columns} filterable={true} />
     }
 }
